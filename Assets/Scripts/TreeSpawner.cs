e77e956701e88c756b06e7dbcb7be9256eec4f60
Kajetan Jóźwiak <unvios22@gmail.com>
2020-04-18 21:35:01 +0200

    Add trees

---
+++ Assets/Scripts/TreeSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TreeSpawner : MonoBehaviour {
+	[SerializeField] private Transform planet;
+	[SerializeField] private Transform treeHolder;
+	[SerializeField] private GameObject[] treePrefabs;
+	[SerializeField] private float treeAmount;
+
+	private void Start() {
+		GenerateTrees();
+	}
+
+	private void GenerateTrees() {
+		var planetMeshFilter = planet.GetComponentInChildren<MeshFilter>();
+		var planetVertices = planetMeshFilter.mesh.vertices;
+
+		for (int i = 0; i < treeAmount; i++) {
+			var vertexLocalPos = planetVertices.ElementAt(Random.Range(0, planetVertices.Length));
+			var spawnPos = planetMeshFilter.transform.TransformPoint(vertexLocalPos);
+			SpawnTree(spawnPos);
+		}
+	}
+
+	private void SpawnTree(Vector3 spawnPos) {
+		var treeToSpawn = treePrefabs.ElementAt(Random.Range(0, treePrefabs.Length));
+		
+		var tree = Instantiate(treeToSpawn, spawnPos, Quaternion.identity, treeHolder);
+		var planetToTreeVector = (spawnPos - planet.position).normalized;
+
+		tree.transform.up = planetToTreeVector;
+	}
+}
