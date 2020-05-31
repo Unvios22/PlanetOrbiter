2326dc2d90020cc8c661d6ba776ac3d5e307cb08
Kajetan Jóźwiak <unvios22@gmail.com>
2020-05-31 18:00:04 +0200

    Fix CameraOrbiter (0,0,0) bug

--- Assets/Scripts/CameraOrbiter.cs
+++ Assets/Scripts/CameraOrbiter.cs
@@ -1,25 +1,24 @@
 ﻿using UnityEngine;
 
 public class CameraOrbiter : MonoBehaviour {
+	[SerializeField] private Transform orbitingTarget;
 
-	[SerializeField] private Transform orbitRelativeTo;
-
-	[SerializeField] private float orbitSpeed;
+	[SerializeField] private float orbitTranslationSpeed;
 	[SerializeField] private float orbitTranslationReactionTime;
 	
-	[SerializeField] private float initialOrbitDistance;
 	[SerializeField] private float orbitDistanceChangeSpeed;
-	[SerializeField] private float orbitDistanceChangeReactTime;
+	[SerializeField] private float orbitDistanceChangeReactionTime;
 
+	[SerializeField] private float initialOrbitDistance;
 	[SerializeField] private float minOrbitDistance;
 	[SerializeField] private float maxOrbitDistance;
 
-	private Vector2 _inputPlanetRotationVector;
-	private float _inputOrbiterDistanceChange;
+	private Vector2 _cameraStrafingMoveVector;
+	private float _inputOrbitingDistanceChange;
 	
-	private Vector2 _mousePosLastFrame;
 	private Vector2 _mousePosThisFrame;
-	private Transform _transform;
+	private Vector2 _mousePosLastFrame;
+	
 	private float _desiredOrbitDistance;
 	private float _currentOrbitDistance;
 	private float _currentOrbitDistanceChangeSpeed;
@@ -27,67 +26,75 @@ public class CameraOrbiter : MonoBehaviour {
 	private Vector2 _desiredOrbitTranslation;
 	private Vector2 _currentOrbitTranslation;
 	private Vector2 _currentOrbitTranslationSpeed;
+	
+	private Transform _transform;
 
 	private void Start() {
+		CacheAndInitializeValues();
+		RepositionAndRealignCameraToTarget();
+	}
+
+	private void CacheAndInitializeValues() {
 		_transform = transform;
 		_currentOrbitDistance = initialOrbitDistance;
 		_desiredOrbitDistance = initialOrbitDistance;
-		RealignCamera();
 	}
 
 	private void Update() {
 		ReadUserInput();
 		ApplyUserInput();
-		RealignCamera();
 	}
 
 	private void ReadUserInput() {
-		ReadPlanetRotationInput();
-		ReadOrbiterDistanceChangeInput();
+		ReadCameraStrafingInput();
+		ReadOrbitingDistanceChangeInput();
 	}
 	
-	private void ReadPlanetRotationInput() {
+	private void ReadCameraStrafingInput() {
 		_mousePosThisFrame = Input.mousePosition;
 		if (!Input.GetKey(KeyCode.Mouse0)) {
-			_inputPlanetRotationVector = Vector2.zero;
+			_cameraStrafingMoveVector = Vector2.zero;
 		} else {
-			_inputPlanetRotationVector = _mousePosLastFrame - _mousePosThisFrame;
+			_cameraStrafingMoveVector = _mousePosLastFrame - _mousePosThisFrame;
 		}
 		_mousePosLastFrame = _mousePosThisFrame;
 	}
 
-	private void ReadOrbiterDistanceChangeInput() {
-		_inputOrbiterDistanceChange = -1 * Input.mouseScrollDelta.y;
+	private void ReadOrbitingDistanceChangeInput() {
+		_inputOrbitingDistanceChange = -1 * Input.mouseScrollDelta.y;
 	}
 	
 	private void ApplyUserInput() {
-		ApplyPlanetRotationInput();
-		ApplyOrbitDistanceChangeInput();
+		ApplyCameraStrafingInput();
+		CalculateOrbitDistance();
+		RepositionAndRealignCameraToTarget();
 	}
 
-	private void ApplyPlanetRotationInput() {
-		//todo refactor
+	private void ApplyCameraStrafingInput() {
 		var currentToMaxOrbitDistanceRatio = _currentOrbitDistance / maxOrbitDistance;
 		_desiredOrbitTranslation =
-			_inputPlanetRotationVector * (Time.deltaTime * orbitSpeed * currentToMaxOrbitDistanceRatio);
+			_cameraStrafingMoveVector * (Time.deltaTime * orbitTranslationSpeed * currentToMaxOrbitDistanceRatio);
 		
 		_currentOrbitTranslation = Vector2.SmoothDamp(_currentOrbitTranslation, _desiredOrbitTranslation,
 			ref _currentOrbitTranslationSpeed, orbitTranslationReactionTime);
 		
 		_transform.Translate(_currentOrbitTranslation);
-		//todo some wonky float calculations going on with _current and _desired. See their values during runtime.
 	}
 
-	private void ApplyOrbitDistanceChangeInput() {
-		_desiredOrbitDistance += _inputOrbiterDistanceChange * orbitDistanceChangeSpeed;
+	private void CalculateOrbitDistance() {
+		_desiredOrbitDistance += _inputOrbitingDistanceChange * orbitDistanceChangeSpeed;
 		_desiredOrbitDistance = Mathf.Clamp(_desiredOrbitDistance, minOrbitDistance, maxOrbitDistance);
 		_currentOrbitDistance = Mathf.SmoothDamp(_currentOrbitDistance, _desiredOrbitDistance,
-			ref _currentOrbitDistanceChangeSpeed, orbitDistanceChangeReactTime);
+			ref _currentOrbitDistanceChangeSpeed, orbitDistanceChangeReactionTime);
 	}
 
-	private void RealignCamera() {
-		var vectorTowardsPlanet = (orbitRelativeTo.position - _transform.position).normalized;
-		_transform.position = vectorTowardsPlanet * (-1 * _currentOrbitDistance);
-		_transform.rotation = Quaternion.LookRotation(vectorTowardsPlanet, _transform.up);
+	private void RepositionAndRealignCameraToTarget() {
+		var targetPosition = orbitingTarget.position;
+		var vectorTowardsTarget = (targetPosition - _transform.position).normalized;
+		
+		var targetToDesiredOrbitPositionVector = vectorTowardsTarget * (-1 * _currentOrbitDistance);
+		
+		_transform.position = targetPosition + targetToDesiredOrbitPositionVector;
+		_transform.rotation = Quaternion.LookRotation(vectorTowardsTarget, _transform.up);
 	}
 }
